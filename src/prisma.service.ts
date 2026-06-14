627f0362e33527e22cb73f3a3189b579a922bfaf
marcoscardenasdev <marcos.cardenas.dev@gmail.com>
2026-06-13 21:02:50 -0600

    feat: update environment configuration and Prisma service to use env variables

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -3,10 +3,12 @@ import { Injectable } from '@nestjs/common';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
 import { PrismaClient } from 'generated/prisma/client';
 
+import { envs } from './config';
+
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL });
+    const adapter = new PrismaBetterSqlite3({ url: envs.databaseUrl });
     super({ adapter });
   }
 }
