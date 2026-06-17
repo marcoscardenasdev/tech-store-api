fd997725db5fb3be53afbb1980ba1e179175f8a0
marcoscardenasdev <marcos.cardenas.dev@gmail.com>
2026-06-17 13:08:37 -0600

    feat: update database schema to include Category and Review models, and modify Product model with categoryId relation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, CategoriesModule, ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
